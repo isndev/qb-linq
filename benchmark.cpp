905bc232452e9437501904cd617de5766079bb59
mohamed belhadi <contact@isndev.com>
2017-04-02 18:59:56 +0200

    idev-3: small improvements

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,9 +1,15 @@
-#include "linq/linq.h"
+#include "Header.h"
 
-bool test_test(int a)
+//template<class _ContType, class _Pred>
+//Linq::Select<_ContType, _Pred> select(_ContType &cont, _Pred &&pr)
+//{
+//	return Linq::Where<_ContType, _Pred>(cont, std::forward<_Pred>(pr));
+//}
+
+bool test_test(int &a)
 {
 	a += 10;
-	return true;
+	return false;
 }
 
 
@@ -25,19 +31,31 @@ int main(int argc, char *argv[])
 {
 	std::vector<int>::iterator it;
 
-	std::vector<int> vec = { 1 , 2 , 1 , 2 , 1, 3, 5, 1, 2 };
-	std::vector<int> vec2 = { 1 };
-	int i = 3;
-	auto const bite = [i](int  &a) { return a == 1; };
-	auto const poil = [i](int  &a) { return a < i; };
+	std::vector<int> vec = { 2, 1 , 2 , 1 , 2 , 1, 3, 5, 1, 2 };
+	std::vector<double> vec2 = { 1., 2., 3.};
+	int i = 4;
+	auto bite = [i](int  &a) { return a == 1; };
+	auto poil = [i](int  &a) { return a < i; };
 	//auto where1 = where(vec, &test_test);
-	auto where2 = where(vec, poil);
-	auto where3 = where2.where(bite);
-	auto where4 = where3;
-	for (auto it = where3.begin(); it != where3.end(); ++it)
+	auto where2 = where(vec, [i](int  &a) { return a < i; });
+	auto where3 = where2.where([i](int  &a) { return a <= 2; })
+						.where([i](int &val) {return val == i-3;  });// .to<std::vector<int>>(vec2);
+	//auto where4 = where3;
+	for (auto it : where3)
 	{
-		std::cout << *it << std::endl;
+		std::cout << it << std::endl;
 	}
+
+	auto where_d = where(vec2, [](double  &a) { return a <= 3.; }).where([](double  &a) { return a == 1.; });
+
+	for (auto it : where_d)
+	{
+		std::cout << it << std::endl;
+	}
+
+	//auto select_s = where3.select<double>([] (int &value) -> double {
+	//	return value / 2;
+	//});
 
 	//for (auto &&a : where3)
 	//{
--- include/linq/linq.h
+++ include/linq/linq.h
@@ -11,151 +11,219 @@
 
 namespace Linq
 {
-	template<class _ContType, class _Pred>
-	class Where
+	template<typename _Handle, typename Iterator>
+	class iterator : public Iterator
 	{
+	protected:
+		_Handle const &_handle;
 	public:
-		using Type = Where<_ContType, _Pred>;
-
-		typedef typename _ContType::iterator iterator_type;
-		typedef typename _ContType::value_type value_type;
-		typedef typename _ContType::const_iterator const_iterator_type;
-		class where_iterator : public iterator_type
-		{
-		private:
-			iterator_type const &_end;
-
-			Where<_ContType, _Pred> const &_context;
-		public:
-			where_iterator() = delete;
-			where_iterator(where_iterator const &rhs)
-				: iterator_type(static_cast<iterator_type>(rhs)), _end(rhs._end), _context(rhs._context)
-			{
-			}
-
-			where_iterator(iterator_type const &rhs, iterator_type const &end, Where<_ContType, _Pred> const &ctxt)
-				: iterator_type(rhs), _end(end), _context(ctxt)
-			{
-			}
-
-			where_iterator(where_iterator const &rhs, Where<_ContType, _Pred> const &ctxt)
-				: iterator_type(rhs), _end(rhs._end), _context(ctxt)
-			{}
-
-			where_iterator &operator++()
-			{
-				iterator_type::operator++();
-				while (static_cast<iterator_type>(*this) != _end && !_context._pred(*static_cast<iterator_type>(*this)))
-					iterator_type::operator++();
-				return (*this);
-			}
-
-			where_iterator operator++(int)
-			{
-				auto itmp = *this;
-				return ++itmp;
-			}
-
-			value_type operator*()
-			{
-				return *static_cast<iterator_type>(*this);
-			}
-		};
-		//typedef std::function_tr  decltype(functor) lambda_type;
+		typedef typename Iterator::value_type value_t;
 
 
-		Where() = delete;
-		Where(Type const &rhs)
-			: _pred(rhs._pred), _container(rhs._container), _end(rhs._end), _begin(rhs._begin)
-		{
-
-		}
-		Where(_ContType &_cont, _Pred const &_pr)
-			: _pred(_pr), _container(_cont), _end(_cont.end(), _cont.end(), *this), _begin(best_start(_cont.begin()), _cont.end(), *this)
+		iterator() = delete;
+		iterator(iterator const &) = default;
+		iterator(Iterator const rhs, _Handle const &handle)
+			: Iterator(rhs), _handle(handle)
 		{}
 
-		where_iterator begin() const
+		~iterator() = default;
+		
+		inline value_t operator*() const { return *static_cast<Iterator const &>(*this); }
+		inline value_t &operator*() { return *static_cast<Iterator &>(*this); }
+		inline bool operator!=(iterator const &rhs) const
 		{
-			return _begin;
-		}
-		where_iterator end() const { return _end; }
-
-		template<class _PredRec>
-		auto where(_PredRec const & next_pred) const
-		{
-			auto &pred = _pred;
-			auto new_pred = [pred, &next_pred](value_type &val) -> bool
-			{
-				return pred(val) && next_pred(val);
-			};
-
-			return Where<_ContType, decltype(new_pred)>(_container, new_pred);
+			return static_cast<Iterator const &>(*this) != static_cast<Iterator const &>(rhs);
 		}
 
-		//template <typename T>
-		//to()
+		iterator &operator++()
+		{
+			do {
+				Iterator::operator++();
+			} while (!static_cast<bool>(_handle(*this)));
+			return *this;
+		}
+	};
+
+	//template<typename _Handle, typename Iterator>
+
+
+
+	template<class _Handle, class _Container>
+	class Object
+	{
+	public:
+		typedef typename _Container::iterator base_iterator_t;
+		typedef typename _Container::value_type value_t;
+		typedef typename std::function<bool(value_t &)> lambda_t;
+		typedef typename iterator<_Handle, base_iterator_t> iterator_t;
+	protected:
+
+		_Container &_container;
+		lambda_t const _predicate;
+		iterator_t const _begin;
+		iterator_t const _end;
+
+		Object() = delete;
+		Object(Object const &) = default;
+		//Object(_Container &cont, lambda_t const &pred)
+		//	: _container(cont), _predicate(pred), _begin(begin), _end(end)
 		//{
-		//	
+		//}
+		//Object(_Container &cont, lambda_t const &&pred, iterator_t const begin, iterator_t const end)
+		//	: _container(cont), _predicate(std::forward<lambda_t>(pred)), _begin(begin), _end(end)
+		//{
 		//}
 
-	private:
-		friend class where_iterator;
-
-		_Pred const &_pred;
-		_ContType &_container;
-		where_iterator const _end;
-		where_iterator const _begin;
-
-		iterator_type const best_start(iterator_type begin) const
+		Object(_Container &cont, lambda_t const &pred, iterator_t const begin, iterator_t const end)
+			: _container(cont), _predicate(pred), _begin(begin), _end(end)
 		{
-			while (begin != _end && !_pred(*begin))
-				++begin;
-			return begin;
+		}
+		~Object() = default;
+
+	public:
+
+		bool operator()(base_iterator_t &it) const
+		{
+			return it == static_cast<base_iterator_t const &>(_end) || _predicate(*it);
+		}
+
+		iterator_t begin() { return _begin; }
+		iterator_t end() { return _end; }
+
+		template<typename _ContainerOut>
+		_ContainerOut &to(_ContainerOut &out) const
+		{
+			std::copy(_begin, _end, std::back_inserter(out));
+			return out;
+		}
+
+		template<typename _ContainerOut = _Container>
+		_ContainerOut to() const
+		{
+			_ContainerOut out;
+			std::copy(_begin, _end, std::back_inserter(out));
+			return std::forward<_ContainerOut>(out);
+		}
+	};
+
+	//template<class _Container, typename _Out_t>
+	//class Select : public Object<Select<_Container, _Out_t>, _Container>
+	//{
+
+	//public:
+
+
+
+	//	typedef typename std::vector<_Out_t>::iterator base_iterator_t;
+	//	typedef typename _Container::value_type value_type;
+	//	typedef typename Linq::Object<Select<std::vector<_Out_t>, _Out_t>, std::vector<_Out_t>> base_t;
+	//	typedef typename Linq::iterator<Select<std::vector<_Out_t>, _Out_t>, base_iterator_t>::iterator iterator_t;
+
+	//	typedef typename std::function<_Out_t(value_type &)> inserter_t;
+
+	//	Select() = delete;
+
+	//	Select(Select const &rhs)
+	//		: base_t(*new std::vector<_Out_t>(rhs._container), rhs._predicate, rhs._begin, rhs._end)
+	//	{
+	//	}
+
+
+	//	//bool operator()(base_iterator_t &it)
+	//	//{
+	//	//	return it == static_cast<base_iterator_t const &>(_end);
+	//	//}
+
+	//	Select(_Container &cont, inserter_t const &pred)
+	//		: base_t(cont,
+	//			[](value_type &) -> bool { return true; },
+	//				iterator_t(_container.begin(), *this),
+	//				iterator_t(_container.end()  , *this)),
+	//		_inserter(pred)
+	//	{
+	//		//for (auto it : cont)
+	//		//	_container.push_back(pred(it));
+	//	}
+
+	//	~Select() { delete &_container; }
+
+	//	//auto where(_Pred const & next_pred) const
+	//	//{
+	//	//	auto pred = _predicate;
+	//	//	_Pred new_pred([pred, next_pred](value_t &val) -> bool
+	//	//	{
+	//	//		OutType 
+
+	//	//		return pred(val) && next_pred(val);
+	//	//	});
+
+	//	//	return Where<std::vector<Out_t>, decltype(new_pred)>(_container, new_pred);
+	//	//}
+	//private:
+	//	inserter_t const _inserter;
+	//};
+
+
+	template<class _Container>
+	class Where : public Object<Where<_Container>, _Container>
+	{
+	public:
+
+		typedef typename _Container::iterator base_iterator_t;
+		typedef typename _Container::value_type value_type;
+
+		typedef typename Linq::Object<Where<_Container>, _Container> base_t;
+		typedef typename Linq::iterator<Where<_Container>, base_iterator_t> iterator_t;
+
+		typedef typename std::function<bool(value_type &)> lambda_t;
+
+		Where() = delete;
+		Where(Where const &rhs)
+			: base_t(rhs._container, rhs._predicate, rhs._begin, rhs._end)
+		{
+		}
+
+		Where(_Container &cont, lambda_t const &pred)
+			: base_t(cont, pred,
+				iterator_t(std::find_if(cont.begin(), cont.end(), pred), *this),
+				iterator_t(cont.end(), *this))
+		{
+		}
+
+		template<typename OutType>
+		auto select(std::function<OutType(value_t &)> const &next_pred)
+		{
+			return Select<Where<_Container>, OutType>(*this, next_pred);
+		}
+
+		auto where(lambda_t const & next_pred) const
+		{
+			auto pred = _predicate;
+			lambda_t new_pred([pred, next_pred](value_type &val) -> bool
+			{
+				return pred(val) && next_pred(val);
+			});
+
+			return Where<_Container>(_container, new_pred);
 		}
 
 	};
 }
 
-
-//template<typename &_ContainerType, typename _Pred>
-//class ILinq
+//template<class _Container, class _Pred, typename _Out_t>
+//Linq::Select<_Container, _Pred, _Out_t> select(_Container &cont, _Pred const &&pr)
 //{
-//	_ContainerType _container;
-//	_Pred _predicate;
-//public:
-//	template<typename T, typename From>
-//	void to()
-//	{
-//		//for (auto it : _container)
-//	}
-//};
-//
-//template<class &_ContainerType>
-//class From
-//{
-//
-//};
-//
-//template<typename T, class Container>
-//class Select
-//{
-//	Container const &_container;
-//public:
-//
-//	Select(Container const &, )
-//
-//	template<typename 
-//
-//	template<typename T>
-//	void select<>
-//
-//};
+//	return Linq::Select<_Container, _Pred>(cont, std::forward<_Pred const>(pr));
+//}
 
-
-
-// from().select()
-template<class _ContType, class _Pred>
-Linq::Where<_ContType, _Pred> where(_ContType &cont, _Pred const &pr)
+template<class _Container, class _Pred>
+Linq::Where<_Container> where(_Container &cont, _Pred const &&pr)
 {
-	return Linq::Where<_ContType, _Pred>(cont, pr);
+	return Linq::Where<_Container>(cont, std::forward<_Pred const>(pr));
+}
+
+template<class _Container, class _Pred>
+Linq::Where<_Container> where(_Container &cont, _Pred const &pr)
+{
+	return Linq::Where<_Container>(cont, std::function<bool(_Container::reference)>(pr));
 }
